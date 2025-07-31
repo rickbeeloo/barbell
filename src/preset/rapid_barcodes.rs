a8ac382cafeb84f6473f735dd05a18005296081b
rickbeeloo <r.beeloo@outlook.com>
2025-07-31 12:23:15 +0200

    fix example patterns to match new Ftag/Rtag

--- src/preset/rapid_barcodes.rs
+++ src/preset/rapid_barcodes.rs
@@ -53,10 +53,9 @@ pub fn demux_rapid_barcodes(
 
     // Filter
     println!("\n{}", "Running annotation filter".purple().bold());
-    let pattern1 = pattern_from_str!("Fbarcode[fw, *, @left(0 to 250), >>]");
-    let pattern2 = pattern_from_str!(
-        "Fbarcode[fw, *, @left(0 to 250)]__Fbarcode[fw, *, @prev_left(0 to 250), >>]"
-    );
+    let pattern1 = pattern_from_str!("Ftag[fw, *, @left(0..250), >>]");
+    let pattern2 =
+        pattern_from_str!("Ftag[fw, *, @left(0..250)]__Ftag[fw, *, @prev_left(0..250), >>]");
     filter(
         format!("{output_folder}/annotation.tsv").as_str(),
         format!("{output_folder}/filtered.tsv").as_str(),
