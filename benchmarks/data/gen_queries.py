dba55ab6b383923c1ec89361dc5cfd6ee9721225
rickbeeloo <r.beeloo@outlook.com>
2025-08-22 13:03:47 +0200

    fix auto query gen

--- benchmarks/data/gen_queries.py
+++ benchmarks/data/gen_queries.py
@@ -4,14 +4,15 @@ rapid_left = "GCTTGGGTGTTTAACC"
 rapid_right = "GTTTTCGCATTTATCGTGAAACGCTTTCGCGTTTTTCGTGCGCCGCTTCA"
 
 # Read rapid bars as fasta
-with open("rapid_queries.fasta", "w") as q:
+with open("rapid_queries.fasta", "w") as q1, open("../../examples/rapid_bars.fasta", "w") as q2:
     for record in fastapy.parse("rapid_bars.txt"):
         barcode_name = record.id
         barcode_seq = record.seq
         # Create query
         query = rapid_left + barcode_seq + rapid_right
         # Create record
-        q.write(f">{barcode_name}\n{query}\n")
+        q1.write(f">{barcode_name}\n{query}\n")
+        q2.write(f">{barcode_name}\n{query}\n")
 
 native_left = "ATTGCTAAGGTTAA"
 native_right = "CAGCACCT"
@@ -26,7 +27,7 @@ def reverse_complement(seq):
     }
     return "".join(trans_table[base] for base in seq[::-1])
 
-with open("native_queries.fasta", "w") as q:
+with open("native_queries.fasta", "w") as q1, open("../../examples/native_bars.fasta", "w") as q2:
     for record in fastapy.parse("native_bars.txt"):
         barcode_name = record.id
         barcode_seq = record.seq
@@ -34,5 +35,6 @@ with open("native_queries.fasta", "w") as q:
         # Create query
         query = native_left + barcode_seq + native_right
         # Create record
-        q.write(f">{barcode_name}\n{query}\n")
+        q1.write(f">{barcode_name}\n{query}\n")
+        q2.write(f">{barcode_name}\n{query}\n")
 
