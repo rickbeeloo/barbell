0ec250b56147a72fed5b8516b5cbaec2cff0ca47
rickbeeloo <r.beeloo@outlook.com>
2025-09-02 09:24:10 +0200

    Changelog + lower default

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+### Changes in new release
+
+- Use new version of Sassy with Mac support (v1.4)
+- Set default cut off to `--min-score=0.2` and `--min-score-diff=0.1` this is loose but good for assembly
+- Use "kits" using `barbell kit --<kit-name>`
+
--- bin/main.rs
+++ bin/main.rs
@@ -94,11 +94,11 @@ enum Commands {
         verbose: bool,
 
         /// Barcode: fraction compared to 'perfect' match score for top candidate
-        #[arg(long = "min-score", default_value_t = 0.5)]
+        #[arg(long = "min-score", default_value_t = 0.2)]
         min_score: f64,
 
         /// Barcode: fraction difference between top 2 candidates
-        #[arg(long = "min-score-diff", default_value_t = 0.05)]
+        #[arg(long = "min-score-diff", default_value_t = 0.1)]
         min_score_diff: f64,
     },
     /// Filter annotation files based on pattern
@@ -250,11 +250,11 @@ enum Commands {
         verbose: bool,
 
         /// Fraction compared to 'perfect' match score for top candidate
-        #[arg(long = "min-score", default_value_t = 0.5)]
+        #[arg(long = "min-score", default_value_t = 0.2)]
         min_score: f64,
 
         /// Fraction difference between top 2 candidates
-        #[arg(long = "min-score-diff", default_value_t = 0.05)]
+        #[arg(long = "min-score-diff", default_value_t = 0.1)]
         min_score_diff: f64,
 
         /// Flank maximum erors in flank, ONLY set manually when you know what you are doing
