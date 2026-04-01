bd92ba41c55420b31fd0ca1f15579194a86a56b0
rickbeeloo <r.beeloo@outlook.com>
2026-04-01 15:07:25 +0200

    clean up cigar parse

--- src/annotate/cigar_parse.rs
+++ src/annotate/cigar_parse.rs
@@ -2,40 +2,6 @@ use pa_types::CigarOp;
 use pa_types::Pos;
 use sassy::*;
 
-/// Map a pattern slice to the corresponding text slice.
-/// Returns Some(((pat_start, pat_end), (text_start, text_end)))
-/// or None if the pattern slice does not appear in the alignment.
-pub fn map_pat_to_text(
-    m: &Match,
-    p_start: i32,
-    p_end: i32,
-) -> Option<((usize, usize), (usize, usize))> {
-    let mut start_pair: Option<Pos> = None;
-    let mut end_pair: Option<Pos> = None;
-
-    let path = m.to_path();
-
-    for &Pos(i, j) in path.iter() {
-        if i >= p_start && i < p_end {
-            if start_pair.is_none() {
-                start_pair = Some(Pos(i, j));
-            }
-            end_pair = Some(Pos(i, j));
-        }
-    }
-
-    match (start_pair, end_pair) {
-        (Some(Pos(pi, pj)), Some(Pos(ei, ej))) => {
-            // convert to half-open ranges
-            Some((
-                (pi as usize, (ei + 1) as usize),
-                (pj as usize, (ej + 1) as usize),
-            ))
-        }
-        _ => None,
-    }
-}
-
 /// Given a match and the
 pub fn map_pat_to_text_with_cost(
     m: &Match,
@@ -145,24 +111,6 @@ mod test {
     }
 
     #[test]
-    fn test_mapping_substring_in_cigar() {
-        let p = b"AAAAACCCAAAA";
-        let t = b"GGGGAAAAACCCAAAAGGGGG";
-        let mut searcher = Searcher::<Iupac>::new_rc();
-        let matches = searcher.search(p, &t, 0);
-        let m = matches.first().unwrap();
-        let ((ps, pe), (t_start, t_end)) = map_pat_to_text(m, 5, 7 + 1).unwrap();
-        let match_slice = &t[t_start..t_end];
-        println!(
-            "Text slice: {:?}",
-            String::from_utf8_lossy(&t[t_start..t_end])
-        );
-        assert_eq!(ps, 5);
-        assert_eq!(pe, 8);
-        assert!(match_slice == b"CCC");
-    }
-
-    #[test]
     fn test_cost_extraction_no_edits() {
         let p = b"AAAAACCCAAAA";
         let t = b"GGGGAAAAACCCAAAAGGGGG";
@@ -263,25 +211,5 @@ mod test {
     //     let ((ps, pe), (t_start, t_end), cost) =
     //         map_pat_to_text_with_cost(m, 5, 7 + 1, false).unwrap();
     //     println!("Cost: {}", cost);
-    // }
-
-    #[test]
-    fn test_mapping_substring_in_cigar_left_overhang() {
-        let p = b"TTTCCCAAAA";
-        let t = b"CAAAAGGGGG";
-        let mut searcher = Searcher::<Iupac>::new_rc_with_overhang(0.5);
-        let matches = searcher.search(p, &t, 3);
-        let m = matches.first().unwrap();
-        let ((ps, pe), (t_start, t_end)) = map_pat_to_text(m, 3, 5 + 1).unwrap();
-        let match_slice = &t[t_start..t_end];
-        println!(
-            "Text slice: ({}, {}) - {:?}",
-            t_start,
-            t_end,
-            String::from_utf8_lossy(&t[t_start..t_end])
-        );
-        assert_eq!(ps, 5);
-        assert_eq!(pe, 6);
-        assert!(match_slice == b"C");
-    }
+    //
 }
